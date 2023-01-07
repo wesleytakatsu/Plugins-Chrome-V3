fe1630b149824efc258c76b8e0495f06cb913b96
Wesley Sieiro Takatsu de Araujo <23043537+wesleytakatsu@users.noreply.github.com>
2023-01-07 15:21:55 -0300

    Configurando tecnicos

---
+++ takatsu-projects-tecnicos/scripts/script.js
@@ -0,0 +1,10 @@
+// https://anobjectisa.com/?p=410
+
+chrome.runtime.onInstalled.addListener(() => {
+    //  ESTE CÓDIGO JAVASCRIPT RODA APENAS UMA VEZ QUANDO
+    //  SE INSTALA O PLUGIN A NÃO SER QUE ELE SEJA REINSTALADO
+    //  ÓTIMO PARA FAZER REGISTROS E CONFIGURAÇÕES INICIAIS
+});
+
+
+
